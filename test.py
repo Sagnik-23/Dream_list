28ca8fa1f7a028b8d552c0b300eb72f2ee5c98b0
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-10-23 12:47:30 +0530

    Test

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+API_KEY = "6ba1f1149fcaab82cbfb0f993c5b3bba"
+
+name = input("Enter movie name: ")
+
+response = requests.get(
+    f"https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key={API_KEY}")
+
+data = response.json()
+
+movie_title = data['results'][0]['original_title']
+movie_desc = data['results'][0]['overview']
+print(movie_title)
+print(movie_desc)
