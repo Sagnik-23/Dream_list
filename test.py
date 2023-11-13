33dc49f07c2aeffc05e477f33cba06a5f0061346
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-11-13 09:30:08 +0530

    Updated test

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import requests
-import json
 
 API_KEY = "6ba1f1149fcaab82cbfb0f993c5b3bba"
 
+
+# Search
 name = input("Enter movie name: ")
 modified_name = name.replace(" ", "+")
 
@@ -15,3 +16,4 @@ movie_title = data['results'][0]['original_title']
 movie_desc = data['results'][0]['overview']
 print(movie_title)
 print(movie_desc)
+# Search end
