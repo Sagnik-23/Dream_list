af756bf69736d99dfa99068cab01dc13edbf6b89
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-11-25 22:56:07 +0530

    Feed function update

--- main.py
+++ main.py
@@ -14,12 +14,29 @@ app = Flask(__name__)
 # Session(app)
 
 
-def get_feed_movies(page_no):
+def get_genres(id):
+    response = requests.get(
+        f"https://api.themoviedb.org/3/genre/movie/list?language=en&api_key={API_KEY}")
+    data = response.json()
+    length = len(data['genres'])
+    for i in range(0, len(data['genres'])):
+        if data['genres'][i]['id'] == id:
+            genre = data['genres'][i]['name']
+            return genre
+
+
+def get_feed_movies(page_no, index):
     url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page_no}&sort_by=popularity.desc&api_key={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    background_image = data['results'][0]['backdrop_path']
+    background_image = data['results'][index]['backdrop_path']
     background_image_url = f"https://image.tmdb.org/t/p/w400{background_image}"
+    title = data['results'][index]['title']
+    overview = data['results'][index]['overview']
+    genre_id = data['results'][index]['genre_ids']
+    genres = []
+    for id in genre_id:
+        genres.append(get_genres(id))
 
 
 @app.route('/')
